c9fe7ff0ddbb7e15a0473028c847eda8d6ec5901
martpedro <32648826+martpedro@users.noreply.github.com>
2019-11-12 14:47:08 -0600

    dia1.1

--- CRM/resources/views/auth/login.blade.php
+++ CRM/resources/views/auth/login.blade.php
@@ -1,73 +1,83 @@
-@extends('layouts.app')
-
+@extends('layouts.pass')
+@section('title','Inicio de Sesión')
+    
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+            <div class="row">
+            <div class="col s12">
+                <div class="container">
+                    <div class="row" id="login-page">
+                        <div class="col s12 m6 l4 z-depth-4 card-panel border-radius-6 login-card bg-opacity-8">
+                            <form class="login-form">
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                        <h5 class="ml-4">
+                                            Sign in
+                                        </h5>
+                                    </div>
                                 </div>
-                            </div>
+                                <div class="row margin">
+                                    <div class="input-field col s12">
+                                        <i class="material-icons prefix pt-2">
+                                            person_outline
+                                        </i>
+                                        <input id="username" type="text">
+                                            <label class="center-align" for="username">
+                                                Username
+                                            </label>
+                                        </input>
+                                    </div>
+                                </div>
+                                <div class="row margin">
+                                    <div class="input-field col s12">
+                                        <i class="material-icons prefix pt-2">
+                                            lock_outline
+                                        </i>
+                                        <input id="password" type="password">
+                                            <label for="password">
+                                                Password
+                                            </label>
+                                        </input>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col s12 m12 l12 ml-2 mt-1">
+                                        <p>
+                                            <label>
+                                                <input type="checkbox"/>
+                                                <span>
+                                                    Remember Me
+                                                </span>
+                                            </label>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                        <a class="btn waves-effect waves-light border-round gradient-45deg-purple-deep-orange col s12" href="index-2.html">
+                                            Login
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="input-field col s6 m6 l6">
+                                        <p class="margin medium-small">
+                                            <a href="user-register.html">
+                                                Register Now!
+                                            </a>
+                                        </p>
+                                    </div>
+                                    <div class="input-field col s6 m6 l6">
+                                        <p class="margin right-align medium-small">
+                                            <a href="user-forgot-password.html">
+                                                Forgot password ?
+                                            </a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
 @endsection
---
+++ CRM/resources/views/layouts/pass.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html class="loading" data-textdirection="ltr" lang="en">
+    <!-- BEGIN: Head-->
+    <head>
+        <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+        <meta content="IE=edge" http-equiv="X-UA-Compatible">
+        <meta content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui" name="viewport">
+        <meta content="Materialize is a Material Design Admin Template,It's modern, responsive and based on Material Design by Google." name="description">
+        <meta content="materialize, admin template, dashboard template, flat admin template, responsive admin template, eCommerce dashboard, analytic dashboard" name="keywords">
+        <meta content="ThemeSelect" name="author">
+        <title>
+        @yield('title')| PromoWeb1's
+        </title>
+        <link href="app-assets/images/favicon/apple-touch-icon-152x152.png" rel="apple-touch-icon">
+        <link href="app-assets/images/favicon/favicon-32x32.png" rel="shortcut icon" type="image/x-icon">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!-- BEGIN: VENDOR CSS-->
+        <link href="app-assets/vendors/vendors.min.css" rel="stylesheet" type="text/css">
+        <!-- END: VENDOR CSS-->
+        <!-- BEGIN: Page Level CSS-->
+        <link href="app-assets/css/themes/vertical-modern-menu-template/materialize.css" rel="stylesheet" type="text/css">
+        <link href="app-assets/css/themes/vertical-modern-menu-template/style.css" rel="stylesheet" type="text/css">
+        <link href="app-assets/css/pages/login.css" rel="stylesheet" type="text/css">
+        <!-- END: Page Level CSS-->
+        <!-- BEGIN: Custom CSS-->
+        <link href="app-assets/css/custom/custom.css" rel="stylesheet" type="text/css">
+            <!-- END: Custom CSS-->
+                                                            
+    </head>
+    <!-- END: Head-->
+    <body class="vertical-layout vertical-menu-collapsible page-header-dark vertical-modern-menu 1-column login-bg blank-page blank-page" data-col="1-column" data-menu="vertical-modern-menu" data-open="click">
+        @yield('content')
+        <!-- BEGIN VENDOR JS-->
+        <script src="app-assets/js/vendors.min.js" type="text/javascript">
+        </script>
+        <!-- BEGIN VENDOR JS-->
+        <!-- BEGIN PAGE VENDOR JS-->
+        <!-- END PAGE VENDOR JS-->
+        <!-- BEGIN THEME  JS-->
+        <script src="app-assets/js/plugins.js" type="text/javascript">
+        </script>
+        <script src="app-assets/js/custom/custom-script.js" type="text/javascript">
+        </script>
+        <!-- END THEME  JS-->
+        <!-- BEGIN PAGE LEVEL JS-->
+        <!-- END PAGE LEVEL JS-->
+    </body>
+</html>
