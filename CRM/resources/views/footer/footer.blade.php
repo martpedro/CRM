dfd4f4e7dca4669790a467819ee83a67c7d4fa10
martpedro <32648826+martpedro@users.noreply.github.com>
2019-11-14 09:57:18 -0600

    dia3-inicio

---
+++ CRM/resources/views/footer/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="page-footer footer footer-static footer-dark gradient-45deg-indigo-purple gradient-shadow navbar-border navbar-shadow">
+      <div class="footer-copyright">
+        <div class="container"><span>&copy; 2019          <a href="https://1.envato.market/pixinvent_portfolio" target="_blank">PIXINVENT</a> All rights reserved.</span><span class="right hide-on-small-only">Design and Developed by <a href="https://1.envato.market/materialize_admin">PIXINVENT</a></span></div>
+      </div>
+    </footer>
